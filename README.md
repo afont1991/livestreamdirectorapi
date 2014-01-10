c63f0915190b64d4dc9ca972efdd5dd21fe3ec75
Andrew Font <font@develop.io>
2014-01-10 05:29:08 -0500

    added error handling for improper livestream_id being used to create a new director

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ### HOW TO USE
  - please add instructions later
+
+
+this is for testing andrews-mbp-2:livestream Afont$ curl -i -X POST -H 'Content-Type: application/json' -d '{"livestream_id": "6488824", "favorite_movies": "Anything by Quentin Tarantino", "favorite_camera": "Canon"}' http://localhost:3000/directors
--- routes/directors.js
+++ routes/directors.js
@@ -26,31 +26,44 @@ exports.getAllDirectors = function(req, res) {
 };
  
 exports.addDirector = function(req, res) {
-	request({
-		uri: "https://api.new.livestream.com/accounts/6488834",
-		method: "GET",
-		timeout: 10000,
-		followRedirect: true,
-		maxRedirects: 10,
-		json: true
-		}, function(error, response, body) {
-			director = new directorModel;
-			director.livestream_id= body['id'];
-			director.full_name = body['full_name'];
-			director.dob = body['dob'];
-			director.favorite_camera = "canon";
-			director.favorite_movies = "Anything by Quentin Tarantino";
-
-			director.save(function(err) {
-				if(err) {
-					console.log(err);
-					response_error = "Error while creating account for Director " + err;
+	livestream_id = req.body.livestream_id;
+	favorite_camera = req.body.favorite_camera;
+	favorite_movies = req.body.favorite_movies;
+	if(!livestream_id){
+		response_error = "livestream ID is required;";
+		APIResponder.respond(res, null, response_error);
+	} else{
+		request({
+			uri: "https://api.new.livestream.com/accounts/" + livestream_id,
+			method: "GET",
+			timeout: 10000,
+			followRedirect: true,
+			maxRedirects: 10,
+			json: true
+			}, function(error, response, body) {
+				if(error || body.name === "NotFoundError" || body.name === "BadRequestError"){ // I included the OR case because your api does not return an error if livestream_id is used with no associtated user
+					response_error = "It appears there was a problem retreiving a director with that livestream_id";
 					APIResponder.respond(res, null, response_error);
 				} else {
-					APIResponder.respond(res, director);
+					director = new directorModel;
+					director.livestream_id= body['id'];
+					director.full_name = body['full_name'];
+					director.dob = body['dob'];
+					director.favorite_camera = favorite_camera;
+					director.favorite_movies = favorite_movies;
+
+					director.save(function(err) {
+						if(err) {
+							console.log(err);
+							response_error = "Error while creating account for Director " + err;
+							APIResponder.respond(res, null, response_error);
+						} else {
+							APIResponder.respond(res, director);
+						}
+					});
 				}
-			});
-	});
+		});
+	}
 };
  
 exports.updateDirector = function(req, res) {
